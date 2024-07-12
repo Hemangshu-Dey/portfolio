db0662c9f7941549c2891c11c9ad7d7ab7bb0179
Hemangshu-Dey <hemangshu1904@gmail.com>
2024-07-12 19:31:37 +0530

    changed scroolbar

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,15 +14,15 @@ const Contact = () => {
             whileInView={{opacity: 1, x: 0}}
             initial={{opacity: 0, x: -100}}
             transition={{duration:1}}
-            className="my-4">{CONTACT.address}</motion.p>
+            className="my-4">{CONTACT.phoneNo}</motion.p>
             <motion.p 
             whileInView={{opacity: 1, x: 0}}
             initial={{opacity: 0, x: 100}}
             transition={{duration:1}}
-            className="my-4">{CONTACT.phoneNo}</motion.p>
-            <a href="#" className="border-b">
+            className="my-4">{CONTACT.email}</motion.p>
+            {/* <a href="#" className="border-b">
                 {CONTACT.email}
-            </a>
+            </a> */}
         </div>
     </div>
   )
